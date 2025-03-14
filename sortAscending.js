850c7e174c35e54ee4d8b11b364fd8a6480f8f2e
Saumya Singh <developersaumya@gmail.com>
2025-03-14 18:45:41 +0530

    Sort
    Bubble Sort algorithm in JavaScript

--- sortAscending.js
+++ sortAscending.js
@@ -1,1 +1,13 @@
+let arr = [11, 5, 8, 1, 4, 9, 3];
+console.log(arr.length)
+for(i=1; i<arr.length; i++)
+{
+  console.log('text')
+  for(j=0; j<arr.length-i; j++) 
+  {
+    temp = arr[i]
+    arr[i] = arr[i+1]
+    arr[i+1] = temp
+  }
 
+}
