6fbda43582a3e5b03df900cf87fa0817e2751dfb
Saumya Singh <developersaumya@gmail.com>
2025-04-26 22:03:38 +0530

    anagram logic

--- anagram.js
+++ anagram.js
@@ -1,1 +1,11 @@
+//can form same word by rearranging the letter of another word
 
+function isAnagram(str1, str2) {
+
+  const formatStr = (str) =>{
+    str.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('')
+  }
+
+  return formatStr(str1) === formatStr(str2);
+}
+console.log(isAnagram('listen1111', 'silent'));
